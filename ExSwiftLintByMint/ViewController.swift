bc2b001ee857a4dda39cda277d16b9b4e4d5a797
jake <palatable7@naver.com>
2021-12-08 00:59:19 +0900

    Initial Commit

---
+++ ExSwiftLintByMint/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ExSwiftLintByMint
+//
+//  Created by 김종권 on 2021/12/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
